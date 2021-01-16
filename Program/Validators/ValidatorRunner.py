5399eec45cfe3051b2adb33e97f0c0e90868fe04
Christa Bridges <christa@pebblepad.co.uk>
2021-01-16 13:26:44 +0000

    number-of-classes-validator ValidatorRunner.py has been through style50.

--- Program/Validators/ValidatorRunner.py
+++ Program/Validators/ValidatorRunner.py
@@ -1,32 +1,39 @@
 from Validators.EqualsSpaceMissingValidator import EqualsSpaceMissingValidator
 from Validators.VarUseValidator import VarUseValidator
 from Validators.NumberOfClassesValidator import NumberOfClassesValidator
+
+
 class ValidatorRunner:
     """
         The class that is responsible for running all the validators.
     """
+
     def run_validators(self, code_box_lines):
         """
             Runs the validators.
-            Args: 
-                code_box_lines: list, all the lines that are in the box 
+            Args:
+                code_box_lines: list, all the lines that are in the box
                 where the user inputs code.
             Returns:
-                error_dictionary: dictionary, contains the errors and 
+                error_dictionary: dictionary, contains the errors and
                 how many there are.
         """
         error_dictionary = {
             "error_list": [],
             "error_count": 0
         }
+
+        # List of all the validators
         validators = [
             EqualsSpaceMissingValidator(),
             VarUseValidator(),
             NumberOfClassesValidator()
         ]
 
+        # Calls each validator and runs them
         for validator in validators:
             validator_results = validator.validate(code_box_lines)
+            # Add the results of each individual validator to the whole one
             error_dictionary["error_list"].append(validator_results["error_list"])
             error_dictionary["error_count"] += validator_results["error_count"]
 
