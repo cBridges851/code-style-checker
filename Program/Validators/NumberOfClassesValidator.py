427fd5d1fcf81650db8b796bd6c10b7d6a53a0ac
Christa Bridges <christa@pebblepad.co.uk>
2021-01-16 13:16:53 +0000

    number-of-classes-validator InterfaceRenderer.py and NumberOfClassesValidator.py have been through style50

--- Program/Validators/NumberOfClassesValidator.py
+++ Program/Validators/NumberOfClassesValidator.py
@@ -12,7 +12,7 @@ class NumberOfClassesValidator:
             Args:
                 lines: list, all the lines of code to check
             Returns:
-                error_dictionary: dictionary, all of the errors that have been found for 
+                error_dictionary: dictionary, all of the errors that have been found for
                 this validator and how many.
         """
 
@@ -20,7 +20,7 @@ class NumberOfClassesValidator:
             "error_list": [],
             "error_count": 0
         }
-        
+
         class_regex = "(?<=class )[A-Za-z]+"
         line_number = 0
         number_of_classes = 0
@@ -29,6 +29,7 @@ class NumberOfClassesValidator:
             classes_on_line = re.findall(class_regex, line)
             line_number += 1
 
+            # If there is at least one class on a line
             if len(classes_on_line) != 0:
                 number_of_classes += 1
 
@@ -36,14 +37,17 @@ class NumberOfClassesValidator:
                     if number_of_classes > 1:
                         error_dictionary["error_count"] += 1
                         error_dictionary["error_list"] \
-                        .append(f"Class Quantity Error: Move the"
-                        + f" {classes_on_line[0]} class to its own file.")
+                            .append(f"Class Quantity Error: Move the"
+                                    + f" {classes_on_line[0]} class"
+                                    + " to its own file.")
                 else:
+                    # For when someone makes multiple classes on a line
                     for i in range(1, len(classes_on_line)):
                         error_dictionary["error_list"] \
-                            .append(f"Class Quantity Error: Move the" 
-                            + f" {classes_on_line[i]} class to its own file.")
+                            .append(f"Class Quantity Error: Move the"
+                                    + f" {classes_on_line[i]} class"
+                                    + " to its own file.")
 
                     error_dictionary["error_count"] += len(classes_on_line) - 1
-        
+
         return error_dictionary
