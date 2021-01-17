a88a4ae282c3bc752b49b2f6fb74609dba396151
Christa Bridges <christa@pebblepad.co.uk>
2021-01-17 16:15:47 +0000

    start-of-signature-space-validator Spacing issue sorted.

--- Program/Validators/StartOfSignatureSpaceValidator.py
+++ Program/Validators/StartOfSignatureSpaceValidator.py
@@ -24,7 +24,7 @@ class StartOfSignatureSpaceValidator:
         loop_start_regex = r"(while|for)\("
         conditional_start_regex = r"(if|else if|switch)\("
         line_number = 0
-        
+
         for line in lines:
             # Check loop signatures
             loop_space_lack_on_line = re.findall(loop_start_regex, line)
@@ -35,17 +35,17 @@ class StartOfSignatureSpaceValidator:
             # Check loop signatures first
             if len(loop_space_lack_on_line) != 0:
                 self.add_error(loop_space_lack_on_line, line_number, error_dictionary)
-            
+
             # Then check conditional statement signatures
             if len(conditional_space_lack_on_line) != 0:
                 self.add_error(conditional_space_lack_on_line, line_number, error_dictionary)
-            
+
         return error_dictionary
-    
+
     def add_error(self, signature_errors, line_number, error_dictionary):
         for error in signature_errors:
             error_dictionary["error_list"] \
-                        .append("Start Of Signature Error: A space is needed"
-                                + f" after the {error}"
-                                + f" on line {line_number}.")
+                .append("Start Of Signature Error: A space is needed"
+                        + f" after the {error}"
+                        + f" on line {line_number}.")
             error_dictionary["error_count"] += 1
