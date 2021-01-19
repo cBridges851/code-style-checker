fc836d8a8209606a1b87669d64ffba5cdabd8aba
Christa Bridges <christa@pebblepad.co.uk>
2021-01-19 13:24:31 +0000

    file-explorer Added documentation.

--- Program/FileHandling/file_logic.py
+++ Program/FileHandling/file_logic.py
@@ -1,7 +1,18 @@
 from tkinter import filedialog
 
 class FileLogic:
+    """
+        The class that does the file handling logic for the application.
+    """
+
     def open_file(self, root):
+        """
+            Allows the user to open a file via the file explorer
+            Args:
+                root: Tk, the root of the Tkinter program.
+            Returns:
+                lines: list, contains all the lines that were in the file.
+        """
         root.filename = filedialog.askopenfilename(
             initialdir="C:/", 
             title="Select a file", 
