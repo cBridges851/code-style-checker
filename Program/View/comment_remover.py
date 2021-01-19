6ff4152a9a7f095ec96f566695efbcf280bfb406
Christa Bridges <christa@pebblepad.co.uk>
2021-01-19 17:05:23 +0000

    comment-remover All issues relating to multiline have been sorted.

--- Program/View/comment_remover.py
+++ Program/View/comment_remover.py
@@ -19,31 +19,28 @@ class CommentRemover:
         # Remove single line comments
         for i in range(len(lines)):
             single_line_comment = re.findall(r"//.+", lines[i])
-            multiline_comment_start = re.findall(r"/*", lines[i])
-            multiline_comment_finish = re.findall(r"*/", lines[i])
+            multiline_comment_start = re.findall(r"/\*.+", lines[i])
+            multiline_comment_finish = re.findall(r".+\*/", lines[i])
 
+            # Removes single line comments
             if len(single_line_comment) != 0:
                 lines[i] = lines[i].replace(single_line_comment[0], "")
             
             # Remove multiline comments
             if not is_multiline_comment:
-                print("Not a multiline comment")
                 # Check if multiline started
                 if len(multiline_comment_start) != 0:
-                    print("Multiline has started")
+                    lines[i] = lines[i].replace(multiline_comment_start[0], "")
                     is_multiline_comment = True
             
                 if len(multiline_comment_finish) != 0:
-                    print("Multiline has ended")
+                    lines[i] = lines[i].replace(multiline_comment_finish[0], "")
                     is_multiline_comment = False
             else:
                 if len(multiline_comment_finish) != 0:
-                    print("Multiline has ended")
+                    lines[i] = lines[i].replace(multiline_comment_finish[0], "")
                     is_multiline_comment = False
                 else:
-                    print("Multiline is still occurring")
+                    lines[i] = ""
 
-        print(lines)
         return lines
-
-CommentRemover().remove_comments(["/* asdisadaidadsj", "console.log('Hello')"])
