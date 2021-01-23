601baf3843dddc631cfb6bb7b2f74035683fdb40
Christa Bridges <christa@pebblepad.co.uk>
2021-01-23 16:41:55 +0000

    code-comments Comments added to code_box_manager.py.

--- Program/View/code_box_manager.py
+++ Program/View/code_box_manager.py
@@ -9,7 +9,9 @@ class CodeBoxManager:
             Args:
                 code_box: ScrolledText, the box where the user inputs their code.
         """
+        # If the code input box contains the placeholder text
         if code_box.get("1.0", tk.END) == "Input JavaScript Code Here...\n":
+            # Clear the box
             code_box.delete("1.0", tk.END)
 
     def insert_placeholder(self, root, code_box):
@@ -18,7 +20,9 @@ class CodeBoxManager:
             Args:
                 code_box: ScrolledText, the box where the user inputs their code.
         """
+        # If the text in the code input box is just a new line, so it is empty
         if code_box.get("1.0", tk.END) == "\n":
+            # Insert the placeholder text
             code_box.insert(tk.END, "Input JavaScript Code Here...")
 
     def display_file_contents(self, root, code_box):
@@ -29,12 +33,15 @@ class CodeBoxManager:
                 root: Tk, the root of the application
                 code_box: ScrolledText, the box where the code will be inserted into.
         """
+        # Opens the file
         lines = FileLogic().open_file(root)
 
+        # If there are lines of code returned from the above line
         if lines is not None:
             # Clear output box
             code_box.delete("1.0", tk.END)
 
-            # Output lines into input box.
+            # Loop through each line of code
             for line in lines:
+                # Insert each line of code into the input box
                 code_box.insert(tk.END, line)
