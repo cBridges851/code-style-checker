3e482feba442d4b2962be7468f7fe9e228911090
Christa Bridges <christa@pebblepad.co.uk>
2021-01-23 17:32:14 +0000

    code-comments Finished making code comments.

--- Program/View/comment_remover.py
+++ Program/View/comment_remover.py
@@ -17,33 +17,53 @@ class CommentRemover:
                 lines: list, the list of lines with the comments removed.
         """
         
+        # Initial value for the variable that switches to True if a multiline 
+        # comment has started 
         is_multiline_comment = False
 
-        # Remove single line comments
+        # Loops through each line
         for i in range(len(lines)):
+            # Looks for single line comments indicated by //
             single_line_comment = re.findall(r"//.+", lines[i])
+            # Looks for the start of a multiline comment indicated by /*, 
+            # with some text potentially after it
             multiline_comment_start = re.findall(r"/\*(.+)?", lines[i])
+            # Looks for the end of a multiline comment indicated by */,
+            # with some text potentially before it
             multiline_comment_finish = re.findall(r"(.+)?\*/", lines[i])
 
-            # Removes single line comments
+            # If there are single line comments on the line
             if len(single_line_comment) != 0:
+                # Replace the comment with an empty string to remove it
                 lines[i] = lines[i].replace(single_line_comment[0], "")
             
-            # Remove multiline comments
+            # Check if the line is currently part of a multiline comment
             if not is_multiline_comment:
                 # Check if multiline started
                 if len(multiline_comment_start) != 0:
+                    # Replace /* and any text after it on the line with an 
+                    # empty string to remove it
                     lines[i] = lines[i].replace(multiline_comment_start[0], "")
+                    # Indicate that the multiline comment has started
                     is_multiline_comment = True
-            
+
+                # In case the multiline comment finishes on the line
                 if len(multiline_comment_finish) != 0:
+                    # Remove */ and any text before it
                     lines[i] = lines[i].replace(multiline_comment_finish[0], "")
+                    # Indicate the multiline comment has finished
                     is_multiline_comment = False
+            # If the line is part of a multiline comment
             else:
+                # If */ is apparent in the line
                 if len(multiline_comment_finish) != 0:
+                    # Remove */ and any text before it
                     lines[i] = lines[i].replace(multiline_comment_finish[0], "")
+                    # Indicate the multiline comment has finished
                     is_multiline_comment = False
                 else:
+                    # Remove all text in the line, multiline comment is still apparent
                     lines[i] = ""
 
+        # Returns the lines with the comments removed
         return lines
--- Program/View/interface_renderer.py
+++ Program/View/interface_renderer.py
@@ -14,24 +14,42 @@ class InterfaceRenderer:
             Initialises the InterfaceRenderer class. It sets up the root and defines
             variables used in multiple places in the rendering.
         """
+        # Initialises the root of the program
         self.root = tk.Tk()
+        # Initialises the title label for the program
         self.title_label = tk.Label(self.root)
+        # Initialises the box where the user will input code
         self.code_box = ScrolledText(self.root)
+        # Initialise the validate button
         self.validate_button = tk.Button(self.root)
+        # Initialise the box where the results will be outputted
         self.output_box = ScrolledText(self.root)
+        # Initialises the menu bar
+        self.menu_bar = tk.Menu(self.root)
+        # Initialises the menu that will be in the menu bar
+        self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        # Sets the main background colour
         self.primary_bg_colour = "#1D1D1D"
+        # Sets the second background colour
         self.secondary_bg_colour = "#2D2D2D"
+        # Sets the colour of the text
         self.box_font_colour = "#C2C0C0"
+        # Sets the width the boxes need to be
         self.box_width = 50
+        # Sets the font and its size for text in the boxes
         self.box_font = ("Consolas 14")
 
     def render_window(self):
         """
             Creates the root window for the application.
         """
+        # Sets the icon window
         self.root.iconbitmap("view/favicon.ico")
+        # Sets the title for the application
         self.root.title("Chrispy Code Style Checker")
+        # Makes the window not resizable
         self.root.resizable(False, False)
+        # Add a background colour to the root and padding on x and y axes
         self.root.configure(
             bg=self.primary_bg_colour,
             padx=10,
@@ -39,31 +57,36 @@ class InterfaceRenderer:
         )
 
     def render_menu_bar(self):
-        menu_bar = tk.Menu(self.root)
-        file_menu = tk.Menu(menu_bar, tearoff=0)
-        file_menu.add_command(
+        # Adds the open file option in the menu
+        self.file_menu.add_command(
             label="Open File",
-            command=lambda: CodeBoxManager().display_file_contents(self.root, self.code_box))
-        file_menu.add_command(label="Exit", command=self.root.quit)
-        menu_bar.add_cascade(label="File", menu=file_menu)
-        self.root.config(menu=menu_bar)
+            command=lambda: CodeBoxManager().display_file_contents(self.root, self.code_box))  # Calls a function that opens the file dialogs
+        # Add the exit option in the menu which will allow the user to close the program
+        self.file_menu.add_command(label="Exit", command=self.root.quit)
+        # Adds the group for opening the file and exiting
+        self.menu_bar.add_cascade(label="File", menu=self.file_menu)
+        # Assigns the menu bar as the root's menu
+        self.root.config(menu=self.menu_bar)
 
     def render_title(self):
         """
             Creates the title of the application.
         """
+        # Style the title
         self.title_label.configure(
             text="Chrispy Code Style Checker",
             bg=self.primary_bg_colour,
             fg="#FFFFFF",
             font=("Helvetica 26 bold")
         )
+        # Put the title on the window
         self.title_label.grid(row=0, column=0)
 
     def render_code_box(self):
         """
             Creates the box that will be used to input code.
         """
+        # Style the code input box
         self.code_box.configure(
             width=self.box_width,
             height=15,
@@ -71,6 +94,7 @@ class InterfaceRenderer:
             fg=self.box_font_colour,
             font=self.box_font
         )
+        # Put the placeholder in the box
         CodeBoxManager().insert_placeholder(self.root, self.code_box)
 
         # Event for when the box is clicked on
@@ -84,25 +108,29 @@ class InterfaceRenderer:
             "<FocusOut>",
             lambda event: CodeBoxManager().insert_placeholder(self.root, self.code_box)
         )
+        # Put the code input box on the window
         self.code_box.grid(row=1, column=0)
 
     def render_validate_button(self):
         """
             Creates the button that will trigger the code to be validated.
         """
+        # Style the validate button
         self.validate_button.configure(
             text="VALIDATE!",
             width=41,
             font=("Helvetica 14"),
             bg="#7A7A7A",
-            command=lambda: Outputter().output_results(self.code_box, self.output_box)
+            command=lambda: Outputter().output_results(self.code_box, self.output_box)  # Triggers the results to be outputted when pressed 
         )
+        # Put the validate button on the window
         self.validate_button.grid(row=2, column=0, pady=10)
 
     def render_output_box(self):
         """
             Creates the box that will be used to display the output.
         """
+        # Style the output box
         self.output_box.configure(
             width=self.box_width,
             height=7,
@@ -112,6 +140,7 @@ class InterfaceRenderer:
             font=self.box_font,
             wrap="word"
         )
+        # Put the output box on the window
         self.output_box.grid(row=3, column=0)
 
     def render_interface(self):
@@ -119,10 +148,17 @@ class InterfaceRenderer:
             Generates the entire user interface by calling all the methods
             that build up each component.
         """
+        # Builds the window
         self.render_window()
+        # Builds the menu bar
         self.render_menu_bar()
+        # Builds the title
         self.render_title()
+        # Builds the code input box
         self.render_code_box()
+        # Builds the validate button
         self.render_validate_button()
+        # Builds the output box 
         self.render_output_box()
+        # Loops the application
         self.root.mainloop()
