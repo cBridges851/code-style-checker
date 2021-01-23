56e79504348b49da4faeaaa271b3fce62e1f271e
Christa Bridges <christa@pebblepad.co.uk>
2021-01-23 16:13:09 +0000

    code-comments Added comments to conditional_statement_signature_quantity_on_line_validator.py.

--- Program/Validators/conditional_statement_signature_quantity_on_line_validator.py
+++ Program/Validators/conditional_statement_signature_quantity_on_line_validator.py
@@ -17,23 +17,30 @@ class ConditionalStatementSignatureQuantityOnLineValidator:
                 this validator and how many.
         """
 
+        # Initial value for the error dictionary
         error_dictionary = {
             "error_list": [],
             "error_count": 0
         }
 
+        # Initial line number
         line_number = 0
+        # Looks for the if, else if, and else keywords
         conditional_statement_regex = r"(if|else if|else)"
 
+        # Loops through every line in the code
         for line in lines:
+            # Increments the line number the user is on
             line_number += 1
 
             # Makes sure there is only one conditional statement keyword on a line
             if len(re.findall(conditional_statement_regex, line)) > 1:
+                # Adds an error if there is more than one conditional statement keyword on a line
                 error_dictionary["error_list"] \
                     .append("Conditional Statement Signatures On Line Quantity Error:"
                             + " There are too many conditional statement"
                             + f" signatures on line {line_number}")
                 error_dictionary["error_count"] += 1
 
+        # Returns the lines and the number of errors after all the lines have been checked
         return error_dictionary
